e416b478e6aa3a19bd591f024cc07d33f3740728
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-07-30 21:01:35 -0400

    more cleanup

--- app.js
+++ app.js
@@ -27,7 +27,6 @@ var connectAssets = require('connect-assets');
 
 var homeController = require('./controllers/home');
 var userController = require('./controllers/user');
-var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
 
 /**
