f85f9391d12dff2bfb6b73a16457e19d4eee0f3d
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-09-01 17:47:48 -0400

    force lowercase on email input + same message if user is not found than if the password is wrong

--- config/passport.js
+++ config/passport.js
@@ -17,8 +17,8 @@ passport.deserializeUser(function(id, done) {
 // Sign in using Email and Password.
 
 passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
-  User.findOne({ email: email }, function(err, user) {
-    if (!user) return done(null, false, { message: 'Email ' + email + ' not found'});
+  User.findOne({ email: email.toLowerCase() }, function(err, user) {
+    if (!user) return done(null, false, { message: 'Invalid email or password.'});
     user.comparePassword(password, function(err, isMatch) {
       if (isMatch) {
         return done(null, user);
