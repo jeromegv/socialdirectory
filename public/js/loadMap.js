dabf4012fd07f46fecbc7f00deb61d7dd6d6aa68
Jérôme Gagnon-Voyer <jerome@gagnonvoyer.com>
2014-10-30 22:32:38 -0400

    add map to organization page

--- public/js/loadMap.js
+++ public/js/loadMap.js
@@ -1,8 +1,8 @@
-function generateVisualization(latitude,longitude){
+function generateVisualization(latitude,longitude,organization){
 	var map = L.map('map', {
         layers: MQ.mapLayer(),
         center: [ latitude, longitude],
-        zoom: 6,
+        zoom: 10,
         minZoom: 5,
         detectRetina:true
     });
@@ -11,6 +11,12 @@ function generateVisualization(latitude,longitude){
 	map.addLayer(marker);
 	//todo show popover on click with address
 	marker.on('click',function (e) {
-		console.log(entry);
+		console.log(e);
+	});
+
+	//there's a bug with leaflet when using inside a bootstrap tab, force a refresh when the tab loads
+	$('a[data-toggle="tab"]').on("shown.bs.tab", function() {
+	    //$("#map").invalidateSize(false);
+	    L.Util.requestAnimFrame(map.invalidateSize,map,!1,map._container);
 	});
 }
